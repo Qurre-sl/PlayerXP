3d00647686be76789405bd00cb59338bf066bb9d
fydne <serverafydne@gmail.com>
2021-05-30 18:33:38 +0500

    updated to qurre v1.4

--- PlayerXP/Plugin.cs
+++ PlayerXP/Plugin.cs
@@ -9,7 +9,7 @@ namespace PlayerXP
         public override string Developer { get; } = "fydne";
         public override string Name { get; } = "PlayerXP";
         public override Version Version { get; } = new Version(1, 0, 0);
-        public override Version NeededQurreVersion { get; } = new Version(1, 3, 9);
+        public override Version NeededQurreVersion { get; } = new Version(1, 4, 0);
         public override void Enable() => RegisterEvents();
         public override void Disable() => UnregisterEvents();
         #endregion
